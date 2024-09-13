50c64ff420d88546b34b2334e542e329a72f2622
AceofStades <rahulpbokade@gmail.com>
2024-09-14 01:34:44 +0530

    Yes

--- gui.py
+++ gui.py
@@ -4,10 +4,8 @@ from model import title_summary, detailed_summary
 
 st.file_uploader("Upload your document:", type=["pdf", "docx", "doc"], accept_multiple_files=False, key="doc", help=None)
 text = st.text_area(label = "Enter your text to summarize", max_chars=2000, placeholder="Max characters: 2000", height=400)
-title = title_summary(text)
-detailed = detailed_summary(text)
 
-def typewriter(text: str, speed: int):
+def typewriter(text: str, speed = 10):
     tokens = text.split()
     container = st.empty()
     for index in range(len(tokens) + 1):
@@ -15,8 +13,8 @@ def typewriter(text: str, speed: int):
         container.markdown(curr_full_text)
         time.sleep(1 / speed)
 
-#Sample Example
-text = "This is an example of streamlit text with typewriter effect :)"
-speed = 10
-typewriter("Title:" + title, speed=speed)
-typewriter("Summary:" + detailed, speed=speed)
+if st.button("Summarize"):
+	title = title_summary(text)
+	detailed = detailed_summary(text)
+	typewriter("Title:" + title)
+	typewriter("Summary:" + detailed)
