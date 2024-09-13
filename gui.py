a60d668de3f6ff8857fa9dd6fac6a805f550b1d3
AceofStades <rahulpbokade@gmail.com>
2024-09-13 14:33:26 +0530

    Minor updates

--- gui.py
+++ gui.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import time
+from model import title_summary, detailed_summary
 
 st.file_uploader("Upload your document:", type=["pdf", "docx", "doc"], accept_multiple_files=False, key="doc", help=None)
+text = st.text_area(label = "Enter your text to summarize", max_chars=2000, placeholder="Max characters: 2000", height=400)
+title = title_summary(text)
+detailed = detailed_summary(text)
 
 def typewriter(text: str, speed: int):
     tokens = text.split()
@@ -14,4 +18,5 @@ def typewriter(text: str, speed: int):
 #Sample Example
 text = "This is an example of streamlit text with typewriter effect :)"
 speed = 10
-typewriter(text=text, speed=speed)
+typewriter("Title:" + title, speed=speed)
+typewriter("Summary:" + detailed, speed=speed)
