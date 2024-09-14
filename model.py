2d4a564f1557a7ccd397c7a6e6e581f1153c0d02
AceofStades <rahulpbokade@gmail.com>
2024-09-15 02:03:00 +0530

    Changed the model to all-mpnet-base-v2

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
-from summarizer import Summarizer
+from summarizer.sbert import SBertSummarizer
 
 def summary(text):
-	model = Summarizer()
+	model = SBertSummarizer('all-mpnet-base-v2')
 	result = model(text, ratio = 0.3)
 	full = ''.join(result)
 	return full
