c0e48b59e9f035de38f08440fa4c6d903b9b9c2d
AceofStades <rahulpbokade@gmail.com>
2024-09-15 02:19:22 +0530

    Initial commit

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from summarizer.sbert import SBertSummarizer
 
 def summary(text):
-	model = SBertSummarizer('all-mpnet-base-v2')
+	model = SBertSummarizer('t5-base')
 	result = model(text, ratio = 0.3)
 	full = ''.join(result)
 	return full
